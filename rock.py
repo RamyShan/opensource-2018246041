272a546ad8ffcf88e3eb26cc241b9661f06f6e87
RamyShan <45709240+RamyShan@users.noreply.github.com>
2018-12-20 16:59:16 +0530

    Add files via upload

---
+++ rock.py
@@ -0,0 +1,12 @@
+user1=raw_input("enter rock,scissors or paper for user1")
+user2=raw_input("enter rock,scissors or paper for user2")
+
+if user1==user2:
+    print"draw"
+elif (user1=="rock") and (user2=="scissors"):
+    print"user1 is the winner"
+elif (user1=="paper") and (user2=="scissors"):
+    print"user2 is the winner"
+
+    
+        
---
+++ sum.py
@@ -0,0 +1,14 @@
+def sum(n1,n2):
+    sum=0
+    i=n1
+    while n2<40:
+        n2=40
+    while i<n2:
+        sum=sum+i
+        i=i+1
+        print sum
+    
+n1=10
+n2=30
+sum(n1,n2)
+    
